e27f1bd891775be0d99ba7f381f9f20593950918
dmkozakov <dimakozakov1999@gmail.com>
2023-10-22 03:04:22 +0300

    Add sprite and palette

---
+++ src/helpers/palette.js
@@ -0,0 +1,14 @@
+export const PALETTE = Object.freeze({
+  textPrimaryColor: '#121417',
+  textInvertColor: ' #FFFFFF',
+  textConditionColor: '#363535',
+  textSecondaryColor: 'rgba(18, 20, 23, 0.5)',
+  accentColor: '#3470FF',
+  activeBgColor: 'rgba(227, 243, 255, 1)',
+  bgMainColor: '#FFFFFF',
+  bgModalColor: 'rgba(18, 20, 23, 0.50)',
+  bgConditionColor: '#f9f9f9',
+  borderSecondaryColor: 'rgba(18, 20, 23, 0.10);',
+  btnHoverColor: '#0B44CD',
+  favoriteBtnBorder: 'rgba(255, 255, 255, 0.8)',
+});
